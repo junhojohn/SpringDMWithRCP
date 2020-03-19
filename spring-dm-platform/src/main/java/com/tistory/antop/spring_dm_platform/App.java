ca0180a011b8752018f1a1792d1cd2350cfc3296
junhojohn <junhojohn@gmail.com>
2020-03-19 09:35:39 +0900

    add spring-dynamic-module basic target platform(osgi version is 3.7.1.R37)

---
+++ spring-dm-platform/src/main/java/com/tistory/antop/spring_dm_platform/App.java
@@ -0,0 +1,13 @@
+package com.tistory.antop.spring_dm_platform;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
