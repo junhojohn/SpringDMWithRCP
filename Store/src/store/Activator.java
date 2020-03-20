46238f2e867950c1247f5f392c0111318ac7bc1b
junhojohn <junhojohn@gmail.com>
2020-03-20 09:31:37 +0900

    1. service.Greeting 제공 클래스: Store 2. service.Greeting 소비 클래스: StoreManager
    1번 제공 클래스에서는 service.Greeting을 확장하는 serviceImpl.GreetingImpl을 함께 export
    시켜서 서비스를 노출시키고 있었음. 그러나 이는 객체지향 측면에서 구체적인 GreetingImpl을 외부에 드러내는 것은 올바른
    구현이 아님. 따라서 serviceImpl 패키지 하위 클래스는 노출하지 않도록 함. 그렇게 되면
    Greeting/GreetingImpl을 소비하던 StoreManager 클래스에서는 GreetingImpl을 인식하지 못해
    컴파일 에러가 생김. 이 문세를 해결하기 위해 제공 클래스에서는 Greeting 인터페이스만 서비스에 등록하여 외부에 노출
    시키고, 소비 클래스에서는 org.osgi.util.tracker 패키지를 이용해서 등록된 서비스를 찾아 사용하는 방식으로
    변경함.

--- Store/src/store/Activator.java
+++ Store/src/store/Activator.java
@@ -3,19 +3,27 @@ package store;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import service.Greeting;
+import service.impl.GreetingImpl;
+
+/**
+ * service.Greeting을 제공하는 Provider 클래스: Store							<br>
+ */
 public class Activator implements BundleActivator {
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	/**
+	 * Store 번들이 시작되면 불리는 콜백 메서드.			<br>
+	 * 시작될 때 서비스에 service.Greeting을 등록한다. 	<br>
 	 */
 	public void start(BundleContext context) throws Exception {
 		System.out.println("Store 번들을 가동 했습니다.");
+		String className = Greeting.class.getName();
+		context.registerService(className, new GreetingImpl(), null);
+		System.out.println("GreetingImpl 객체를" + className + "으로 서비스 레지스트리에 등록했습니다.");
 	}
 	
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	/**
+	 * Store 번들이 멈추면 불리는 콜백 메서드.		<br>
 	 */
 	public void stop(BundleContext context) throws Exception {
 		System.out.println("Store 번들을 멈췄습니다.");
